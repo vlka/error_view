84a872820b3af38bd9fa6861e83f520a1f51b92d
vlka <vladimir_kashkin@bk.ru>
2017-06-08 03:23:07 +0300

    s2

--- ErrorView.php
+++ ErrorView.php
@@ -10,7 +10,7 @@ class ErrorView extends Component implements BootstrapInterface
 {
     public function bootstrap($app)
     {
-        $app->errorHandler->callStackItemView = '@vlka/error_view/callStackItem.php';
+        $app->errorHandler->callStackItemView = '@vlka/yii2-error_view/callStackItem.php';
         Event::on(Module::className(), Module::EVENT_BEFORE_ACTION, function($event){exit(555);
             $event->sender->traceLine = '<a href="phpstorm://open?file={file}&line={line}">{file}:{line}</a>';
         });
