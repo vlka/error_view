a12080b07e6ae9977fefe2a71ec958efa0d00455
vlka <vladimir_kashkin@bk.ru>
2017-06-09 14:49:46 +0300

    Init

--- ErrorView.php
+++ ErrorView.php
@@ -5,14 +5,20 @@ use yii\base\BootstrapInterface;
 use yii\base\Component;
 use yii\base\Event;
 use yii\debug\Module;
+use yii\web\Application;
 
 class ErrorView extends Component implements BootstrapInterface
 {
+    /**
+     * @param \yii\base\Application $app
+     */
     public function bootstrap($app)
     {
-        $app->errorHandler->callStackItemView = '@vlka/errorview/callStackItem.php';
-        Event::on(Module::className(), Module::EVENT_BEFORE_ACTION, function($event){
-            $event->sender->traceLine = '<a href="phpstorm://open?file={file}&line={line}">{file}:{line}</a>';
-        });
+        if($app instanceof Application){
+            $app->errorHandler->callStackItemView = '@vlka/errorview/callStackItem.php';
+            Event::on(Module::className(), Module::EVENT_BEFORE_ACTION, function($event){
+                $event->sender->traceLine = '<a href="phpstorm://open?file={file}&line={line}">{file}:{line}</a>';
+            });
+        }
     }
 }
